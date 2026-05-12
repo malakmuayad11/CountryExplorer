d111becfc9cc8902baaca0fc1a87b8d3e8d1b1d3
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-05-12 15:58:04 +0300

    Edited the tsconfig.json options to allows exporting functions

--- js/storage.ts
+++ js/storage.ts
@@ -1,17 +1,17 @@
 "use strict";
 
-export function addToFavoriteCountries(value) {
+export function addToFavoriteCountries(value: string): void {
   localStorage.setItem("favoriteCountries", value);
 }
 
-export function addToTheme(value) {
+export function addToTheme(value: string): void {
   localStorage.setItem("theme", value);
 }
 
-export function getTheme() {
+export function getTheme(): string {
   return localStorage.getItem("theme");
 }
 
-export function getFavoriteCountries() {
+export function getFavoriteCountries(): string {
   return localStorage.getItem("favoriteCountries");
 }
