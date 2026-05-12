8036b94635819eeac4e025e9b1599551ed49b01a
Malak Abu-Omar <malakmuayad15@gmail.com>
2026-05-12 16:22:55 +0300

    Fixed api.ts missing types

--- js/api.ts
+++ js/api.ts
@@ -1,7 +1,23 @@
 "use strict";
 
-export async function getAllCountries() {
-  const urlAll = new URL("https://restcountries.com/v3.1/all");
+class Country {
+  constructor(
+    public flag: string,
+    public name: string,
+    public region: string,
+    public capital: string[],
+    public population: number,
+    public languages: string[],
+    public borders: string[],
+  ) {
+    capital = [];
+    languages = [];
+    borders = [];
+  }
+}
+
+export async function getAllCountries(): Promise<Country[] | undefined> {
+  const urlAll: URL = new URL("https://restcountries.com/v3.1/all");
   urlAll.searchParams.set(
     "fields",
     "flags,name,region,capital,population,languages,borders",
@@ -12,26 +28,29 @@ export async function getAllCountries() {
     if (!res.ok)
       throw new Error("HTTP error: " + res.status + " " + res.statusText);
     const data = await res.json();
-    return data.map((c) => {
-      return {
-        flag: c.flags.svg,
-        name: c.name.common,
-        region: c.region,
-        capital: c.capital,
-        population: c.population,
-        languages: c.languages,
-        borders: c.borders,
-      };
+
+    return data.map((c: any): Country => {
+      return new Country(
+        c.flags.svg,
+        c.name.common,
+        c.region,
+        c.capital,
+        c.population,
+        c.languages,
+        c.borders,
+      );
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function getCountryByName(name) {
+export async function getCountryByName(
+  name: string,
+): Promise<Country | undefined> {
   if (!name) throw new Error("Please provide a valid country name");
 
-  const urlGet = new URL("https://restcountries.com/v3.1/name");
+  const urlGet: URL = new URL("https://restcountries.com/v3.1/name");
   urlGet.pathname += `/${name}`;
   urlGet.searchParams.set(
     "fields",
@@ -45,24 +64,35 @@ export async function getCountryByName(name) {
       throw new Error("HTTP error: " + res.status + " " + res.statusText);
 
     const data = await res.json();
-    return {
-      flag: data[0].flags.svg,
-      name: data[0].name.common,
-      region: data[0].region,
-      capital: data[0].capital,
-      population: data[0].population,
-      languages: data[0].languages,
-      borders: data[0].borders,
-    };
+    return new Country(
+      data[0].flags.svg,
+      data[0].name.common,
+      data[0].region,
+      data[0].capital,
+      data[0].population,
+      data[0].languages,
+      data[0].borders,
+    );
+    // return {
+    //   flag: data[0].flags.svg,
+    //   name: data[0].name.common,
+    //   region: data[0].region,
+    //   capital: data[0].capital,
+    //   population: data[0].population,
+    //   languages: data[0].languages,
+    //   borders: data[0].borders,
+    // };
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function getByRegion(region) {
+export async function getByRegion(
+  region: string,
+): Promise<Country | undefined> {
   if (!region) throw new Error("Please provide a valid region name");
 
-  const urlGetByRegion = new URL("https://restcountries.com/v3.1/region");
+  const urlGetByRegion: URL = new URL("https://restcountries.com/v3.1/region");
   urlGetByRegion.pathname += `/${region}`;
   urlGetByRegion.searchParams.set(
     "fields",
@@ -74,26 +104,28 @@ export async function getByRegion(region) {
     if (!res.ok)
       throw new Error("HTTP error: " + res.status + " " + res.statusText);
     const data = await res.json();
-    return data.map((c) => {
-      return {
-        flag: c.flags.svg,
-        name: c.name.common,
-        region: c.region,
-        capital: c.capital,
-        population: c.population,
-        languages: c.languages,
-        borders: c.borders,
-      };
+    return data.map((c: any): Country => {
+      return new Country(
+        c.flags.svg,
+        c.name.common,
+        c.region,
+        c.capital,
+        c.population,
+        c.languages,
+        c.borders,
+      );
     });
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function getCountryByCode(code) {
+export async function getCountryByCode(
+  code: string,
+): Promise<Country | undefined> {
   if (!code) throw new Error("Please provide a valid country code");
 
-  const urlGet = new URL("https://restcountries.com/v3.1/alpha");
+  const urlGet: URL = new URL("https://restcountries.com/v3.1/alpha");
   urlGet.pathname += `/${code}`;
   urlGet.searchParams.set(
     "fields",
@@ -107,15 +139,15 @@ export async function getCountryByCode(code) {
       throw new Error("HTTP error: " + res.status + " " + res.statusText);
 
     const data = await res.json();
-    return {
-      flag: data.flags.svg,
-      name: data.name.common,
-      region: data.region,
-      capital: data.capital,
-      population: data.population,
-      languages: data.languages,
-      borders: data.borders,
-    };
+    return new Country(
+      data.flags.svg,
+      data.name.common,
+      data.region,
+      data.capital,
+      data.population,
+      data.languages,
+      data.borders,
+    );
   } catch (error) {
     console.log(error);
   }
